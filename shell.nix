566c9fc094df66e66f45e0d621067503a99f7baa
Eduard Vandenrijd <evandenrijd@gmail.com>
2024-10-28 12:32:08 +0000

    add matrix multiplication

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  shellHook =
+    ''
+      eval "$(/Users/van/miniconda3/bin/conda shell.zsh hook)"
+      make_emacs_info_happy -dir_file=.info/nix-profile/share/info -info_dir=~/.nix-profile/share/info
+      export INFOPATH=$(pwd)/.info/nix-profile/share/info:$INFOPATH
+      zsh
+    '';
+
+  buildInputs = [
+    pkgs.git
+    pkgs.pandoc           # to make a pdf of Jupyter notebooks
+    pkgs.texliveFull      # xelatex, also needed to make a pdf of Jupyter notebooks
+    pkgs.python312
+  ];
+}
